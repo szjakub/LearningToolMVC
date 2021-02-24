55762d642ce540734e052a3defb14754ec9314d0
szjakub <jakubszkodny@gmail.com>
2021-02-24 22:37:37 +0100

    Create migrations

---
+++ LearningToolMVC/Migrations/20210224213516_AddDefinitionToDb.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LearningToolMVC.Migrations
+{
+    public partial class AddDefinitionToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Definitions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Sentence = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Meaning = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Definitions", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Definitions");
+        }
+    }
+}
