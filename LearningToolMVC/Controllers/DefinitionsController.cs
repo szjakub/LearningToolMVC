2ab0989136980b0cfd6f373786ff32be2787c99b
szjakub <jakubszkodny@gmail.com>
2021-02-24 22:37:21 +0100

    Add SQL Server configuration | Create model and controller

---
+++ LearningToolMVC/Controllers/DefinitionsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LearningToolMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningToolMVC.Controllers
+{
+    public class DefinitionsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public DefinitionModel Definition { get; set; }
+
+        public DefinitionsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ LearningToolMVC/Models/DefinitionModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningToolMVC.Models
+{
+    public class DefinitionModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Sentence { get; set; }
+
+        [Required]
+        public string Meaning { get; set; }
+    }
+}
